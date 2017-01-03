be55c03003637021b0bb2cba1056cb8eb15e9480
Magdalena Bialon <magdalenabialon@gmail.com>
2017-01-04 01:39:11 +1100

    changed board logic a bit so that it would be easier to change it in the future if needed

--- commands.rb
+++ commands.rb
@@ -1,5 +1,12 @@
 
 class Robot
+
+  attr_reader :board_size_X, :board_size_Y
+
+  def initialize(board_size_X=5, board_size_Y=5)
+    @board_size_X = board_size_X
+    @board_size_Y = board_size_Y
+  end
   
   def place(x,y,face)
     if (0..5) === x && (0..5) === y && ['NORTH','SOUTH','EAST','WEST'].include?(face)
@@ -8,14 +15,19 @@ class Robot
       @face = face
       [@x,@y,@face]
     else
-      "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west"
+      "X and Y must be in 0 to #{@board_size_X} (for X) and #{@board_size_Y} (for Y) range and face must be north south east or west"
     end
   end
 
 end
 
 
-robot = Robot.new
+
+
+
+
+
+# robot = Robot.new
 # p robot.place(0,0,'NORTH')
 # p robot.place(10,0,'NORTH')
 # p robot.place(1,0,'sth')
--- test_commands.rb
+++ test_commands.rb
@@ -4,19 +4,18 @@ require_relative 'commands'
 
 class TestCommands < Minitest::Test
 
+  @@robot = Robot.new
+
   def test_PLACE_command_returns_simple_output
-    robot = Robot.new
-    assert_equal [1,1,'EAST'] , robot.place(1,1,'EAST')
+    assert_equal [1,1,'EAST'] , @@robot.place(1,1,'EAST')
   end
 
   def test_PLACE_command_not_valid_X_Y
-    robot = Robot.new
-    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west", robot.place(10,-1,'NORTH')
+    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west", @@robot.place(10,-1,'NORTH')
   end
 
   def test_PLACE_command_not_valid_FACE
-    robot = Robot.new
-    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west", robot.place(0,1,'NAH')
+    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west", @@robot.place(0,1,'NAH')
   end
   
 end
