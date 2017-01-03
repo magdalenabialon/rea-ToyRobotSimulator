4ae636e4b6ffd925086051d0a844a53a815b139d
Magdalena Bialon <magdalenabialon@gmail.com>
2017-01-04 01:50:43 +1100

    testing move method

--- test_commands.rb
+++ test_commands.rb
@@ -11,11 +11,16 @@ class TestCommands < Minitest::Test
   end
 
   def test_PLACE_command_not_valid_X_Y
-    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west", @@robot.place(10,-1,'NORTH')
+    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west" , @@robot.place(10,-1,'NORTH')
   end
 
   def test_PLACE_command_not_valid_FACE
-    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west", @@robot.place(0,1,'NAH')
+    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west" , @@robot.place(0,1,'NAH')
+  end
+
+  def test_MOVE_command
+    @@robot.place(1,1,'EAST')
+    assert_equal 2 , @@robot.move
   end
   
 end
