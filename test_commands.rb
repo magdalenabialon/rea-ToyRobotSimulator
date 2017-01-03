dec633ef2e1aa5ab430b721636feb4bdfd98ff62
Magdalena Bialon <magdalenabialon@gmail.com>
2017-01-04 01:22:41 +1100

    second failing test - R

--- test_commands.rb
+++ test_commands.rb
@@ -8,5 +8,10 @@ class TestCommands < Minitest::Test
     robot = Robot.new
     assert_equal [1,1,'EAST'] , robot.place(1,1,'EAST')
   end
+
+  def test_PLACE_command_not_valid_X_Y
+    robot = Robot.new
+    assert_equal "X and Y must be in 0 to 5 (for X) and 5 (for Y) range and face must be north south east or west", robot.place(10,-1,'NORTH')
+  end
   
 end
