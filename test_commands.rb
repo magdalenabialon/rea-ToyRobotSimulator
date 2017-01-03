8130039392031fb7773022273d29756e02badbda
Magdalena Bialon <magdalenabialon@gmail.com>
2017-01-04 01:11:11 +1100

    first failing test for place command - RED

--- test_commands.rb
+++ test_commands.rb
@@ -2,5 +2,9 @@ require 'minitest/autorun'
 
 class TestCommands < Minitest::Test
 
-
+  def test_PLACE_command_returns_simple_output
+    robot = Robot.new
+    assert_equal robot.place(1,1,'EAST') [1,1,'EAST']
+  end
+  
 end
