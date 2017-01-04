b163926563418ea128853fefb609c46cc84a64b1
Magdalena Bialon <magdalenabialon@gmail.com>
2017-01-04 16:05:31 +1100

    trying to test and figure out logic for PERFORM_COMMANDS_method

--- execute_user_commands.rb
+++ execute_user_commands.rb
@@ -1,24 +1,41 @@
+require_relative 'robot_commands'
+
+
 
 class ExecuteUserCommands
 
-  def initialize(user_input)
+  def initialize(toy,user_input)
+    @toy = toy
     @user_input = user_input
+  end
+
+  def perform_commands
+    greet
+  end
+
+  # private
+
+  def greet
+    p 'Welcome to Toy Robot Simulator'
+    p 'Please start with place command'
   end
 
   def get_user_input
     @user_input.save_command
   end
 
+
 end
 
 
 class UserInput
   def save_command
-    gets.chomp.upcase
+    @command = gets.chomp.upcase
   end
 end
 
 
-user_input = UserInput.new
-command = ExecuteUserCommands.new(user_input)
-p command.get_user_input
+# robot = Robot.new
+# user_input = UserInput.new
+# command = ExecuteUserCommands.new(robot,user_input)
+# command.perform_commands
--- test_user_commands.rb
+++ test_user_commands.rb
@@ -7,7 +7,7 @@ class TestExecuteUserCommands < Minitest::Test
 
   class DummyUserInput
     def initialize(user_input)
-      @user_input = user_input.gsub(/\s+/ , '').upcase
+      @user_input = user_input.upcase
     end
 
     def save_command
@@ -16,13 +16,21 @@ class TestExecuteUserCommands < Minitest::Test
   end
 
   def test_execute_user_commands_first_simple_test
+    robot = Robot.new
     user_input = DummyUserInput.new('PLACE')
-    assert_equal 'PLACE' , ExecuteUserCommands.new(user_input).get_user_input
+    assert_equal 'PLACE' , ExecuteUserCommands.new(robot,user_input).get_user_input
   end
 
   def test_execute_user_commands_simple_test_for_random_input
+    robot = Robot.new
     user_input = DummyUserInput.new('something')
-    assert_equal 'SOMETHING' , ExecuteUserCommands.new(user_input).get_user_input
+    assert_equal 'SOMETHING' , ExecuteUserCommands.new(robot,user_input).get_user_input
+  end
+
+  def test_PERFORM_COMMANDS_method
+    robot = Robot.new
+    user_input = DummyUserInput.new('PLACE')
+    assert_equal 'Please start with place command' , ExecuteUserCommands.new(robot,user_input).perform_commands
   end
 
 end
